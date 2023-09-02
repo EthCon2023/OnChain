61af853e6efd78ded0eead976d4e6d839b73a995
Cosmodude <vladislav.lenskii@gmail.com>
2023-09-02 21:42:00 +0900

    introduced axelar

--- contracts/PayLink.sol
+++ contracts/PayLink.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import { AxelarExecutable } from './imports/AxelarExecutable.sol';
 
 contract PayLink {
 
@@ -13,17 +13,18 @@ contract PayLink {
 
     }
 
-    function deposit(uint256 amount, bytes32 link) payable public {
+    function deposit(bytes32 link) payable public {
         //require(msg.value == amount); do I need?
-        LinkToAmount[link] = amount;
+        LinkToAmount[link] = msg.value;
     }
 
-    function withdraw(bytes32 link) public {
+    function withdraw(bytes32 link) public returns(bytes memory){
         require(LinkToAmount[link] > 0);
         uint256 _amount = LinkToAmount[link];
         address _to = payable(msg.sender);
         (bool sent, bytes memory data) = _to.call{value: _amount}("");
         require(sent, "Failed to send Ether");
+        return data;
     }
 
     function getBalance() public view returns (uint256) {
