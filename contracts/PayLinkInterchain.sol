5c4477e7f1457b586d09b46d1b9ccafd483ed07e
Cosmodude <vladislav.lenskii@gmail.com>
2023-09-03 10:08:10 +0900

    added departure function

--- contracts/PayLinkInterchain.sol
+++ contracts/PayLinkInterchain.sol
@@ -4,12 +4,16 @@ pragma solidity 0.8.19;
 import { IERC20 } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IERC20.sol';
 import { AxelarExecutable } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecutable.sol';
 import { IAxelarGateway } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol';
+import { IAxelarGasService } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol';
 
-contract PayLink is AxelarExecutable{
+contract PayLinkInterchain is AxelarExecutable{
+    IAxelarGasService public immutable gasService;
 
-    mapping(bytes32 => uint256 amount) LinkToAmount; 
+    mapping(bytes32 => uint256 amount) private LinkToAmount; 
 
-    constructor(address gateway_) AxelarExecutable(gateway_) {}
+    constructor(address gateway_, address gasReceiver_) AxelarExecutable(gateway_) {
+        gasService = IAxelarGasService(gasReceiver_);
+    }
 
     receive() external payable{
 
@@ -18,6 +22,31 @@ contract PayLink is AxelarExecutable{
     function deposit(bytes32 link) payable public {
         //require(msg.value == amount); do I need?
         LinkToAmount[link] = msg.value;
+    }
+    
+    function sendTokens(
+        string memory destinationChain,
+        string memory destinationAddress,
+        bytes32 link,
+        string memory symbol,
+        uint256 amount
+    ) external payable {
+        require(msg.value > 0, 'Gas payment is required');
+
+        address tokenAddress = gateway.tokenAddresses(symbol);
+        IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount);
+        IERC20(tokenAddress).approve(address(gateway), amount);
+        bytes memory payload = abi.encode(link);
+        gasService.payNativeGasForContractCallWithToken{ value: msg.value }(
+            address(this),
+            destinationChain,
+            destinationAddress,
+            payload,
+            symbol,
+            amount,
+            msg.sender
+        );
+        gateway.callContractWithToken(destinationChain, destinationAddress, payload, symbol, amount);
     }
 
     function withdraw(bytes32 link) public returns(bytes memory){
